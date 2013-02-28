adfb889cd66586f21bd36c9a7ccfc04dea11dc4e
cjones2500 <jonesc@pplxint6.physics.ox.ac.uk>
2013-02-28 17:11:39 +0000

    NI DAQ functions and requirements

---
+++ DAQmxConfig.py
@@ -0,0 +1,26 @@
+import sys
+import platform
+
+if sys.platform.startswith('win'):
+    # Full path of the NIDAQmx.h file
+    # Default location on Windows XP
+    dot_h_file =  r'C:\Program Files (x86)\National Instruments\NI-DAQ\DAQmx ANSI C Dev\include\NIDAQmx.h'
+
+    if platform.release()=='7' and platform.architecture()[0]=='64bit':
+        dot_h_file = r'C:\Program Files (x86)\National Instruments\NI-DAQ\DAQmx ANSI C Dev\include\NIDAQmx.h'
+
+    # Name (and eventually path) of the library
+    # Default on Windows is nicaiu
+    lib_name = "nicaiu"
+
+elif sys.platform.startswith('linux'):
+    # On linux you can use the command find_library('nidaqmx')
+
+    # Full path of the NIDAQmx.h file
+    dot_h_file = '/usr/local/natinst/nidaqmx/include/NIDAQmx.h'
+
+    # Name (and eventually path) of the library
+    lib_name = 'libnidaqmx.so'
+
+else:
+    raise NotImplementedError, "Location of niDAQmx library and include file unknown on %s - if you find out, please let the PyDAQmx project know" % (sys.platform)
