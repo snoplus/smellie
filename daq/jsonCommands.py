0e5ea308146d74ef9650bfdcf4bc9a87bf534ec2
cjones2500 <christopher.jones@balliol.ox.ac.uk>
2013-06-04 16:54:03 +0100

    Building the ORCA SMELLIE front end communications (work in progress)

--- daq/jsonCommands.py
+++ daq/jsonCommands.py
@@ -49,19 +49,42 @@ def getSubRunParameters(subrun):
     subrun_id = dict['hw'][isubrun]['sub_run_id']
     fsInputChannel = dict['hw'][isubrun]['fibre_switch_channel']
     return subrun_id,fsInputChannel
+
+def 
 	
-def StartSubRun(run, subrun):
+def getSubRunParameters(run, subrun):
     file = './SMELLIE_very_short.json'
     dict = ReadJSON(file) 
     irun = returnRun(dict, run)
-    laser_id = dict['run_list'][irun]['laser_selected'][0]
-    number_of_pulses = dict['run_list'][irun]['nb_pulses']
-    pulse_frequency = dict['run_list'][irun]['smellie_pulse_frequency']
+    nameList = ['laser_id','author','time_stamp','make_smellie_calibration_table','smellie_calibration_table_reference','trigger_mode','laser_selected','smellie_deriven','number_of_pulses','pulse_frequency']
+    valueList = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']    
+    valueList[0] = dict['run_list'][irun]['laser_selected'][0]
+    valueList[1] = dict['run_list'][irun]['author']
+    valueList[2] = dict['run_list'][irun]['time_stamp']
+    valueList[3] = dict['run_list'][irun]['make_smellie_calibration_table']
+    valueList[4] = dict['run_list'][irun]['smellie_calibration_table_reference']
+    valueList[5] = dict['run_list'][irun]['trigger_mode']
+    valueList[6] = dict['run_list'][irun]['laser_selected']
+    valueList[7] = dict['run_list'][irun]['smellie_driven']
+    valueList[8] = dict['run_list'][irun]['nb_pulses']
+    valueList[9] = dict['run_list'][irun]['smellie_pulse_frequency']
 	
     isubrun = returnSubRun(dict, subrun)
+    subrun_id = dict['hw'][isubrun]['sub_run_id']
     fsInputChannel = dict['hw'][isubrun]['fibre_switch_channel']
-    print fsInputChannel
+    print "Configuration parameters are:"
+    for i in range(0,10):
+        print str(nameList[i]) + "\t" +  str(valueList[i])
+    print "\nSubRun Parameters are:"
+    print subrun_id,fsInputChannel
+
+def getConfigurationParameters(configId):
+     file = './SMELLIE_config.json'
+     dict = READJSON(file)
+     iconfig = returnRun(dict,configId)
+     nameList = ['config_id','config_rev','time_stamp','tcpip_coummincation_timeouts','safe_laser_switch_output','safe_fibre_switch_output','fibre_switch_serial_port'.'fibre_switch_baud_rate','fibre_switch_from_laser_head_map','fibre_switch_to_detector_fibre_map','laser_switch_wait','laser_head_to_fibre_splitter_map','laser_switch_to_laser_head_map','sepia_laser_driver_module_slotID','sepia_laser_driver_primary_id','sndrop_ip_address','orca_ip_address','orca_server_port','snodrop_client_port','tcpip_communication_max_string_length','snodrop_max_number_listening_connections','detector_database_server_ip_address','NI_device_name','self_test_number_of_pulses','self_test_trigger_frequency','self_test_sampling_frequency','self_test_number_of_samples_per_pulse','self_test_NI_trigger_output_pin','self_test_NI_analogue_input_pin']
+     valueList = [' ',' ',' ',' ',' ',' ',' '.' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+     
     
-StartSubRun(1,1)
-print getRunParameters(2)
-print getSubRunParameters(3)
+GetSubRunParameters(2,1)
+GetConfigurationParameters(1)
